f6415a5f185fbda70e63b493f822e01d3f9431d4
yahyobekanvarov <yahyobekanvarov5@gmail.com>
2025-07-21 09:52:10 +0500

    Update and rename Volume of silindre calculator.py to Volume of cylinder calculator.py

---
+++ Volume of cylinder calculator.py
@@ -0,0 +1,18 @@
+import math
+#Volume of cylinder
+
+height = float(input("Enter the height of a cylinder:"))
+radius = float(input("Enter the radius of a cylinder :"))
+volume = math.pi * pow(radius, 2) * height
+print(f"Volume of the cylinder is : {round(volume, 2)} m^3")
+
+
+
+
+
+
+
+
+
+
+
